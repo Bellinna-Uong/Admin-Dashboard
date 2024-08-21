92978b0acb3f30a94fb7b973d372f4612d61ee64
Bellinna Uong <147825322+Bellinna-Uong@users.noreply.github.com>
2024-08-21 19:29:18 +0200

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,36 @@
+const Orders = [
+    {
+        productName:'Pomodoro - Todo list',
+        productNumber:'43952',
+        paymentStatus:'Due',
+        shipping:'Completed'
+    },
+        
+    {                       
+        productName:'Weather App',
+        productNumber:'39524',
+        paymentStatus:'Due',
+        shipping:'Completed'
+    },
+        
+    {
+        productName:'Data analysis',
+        productNumber:'85631',
+        paymentStatus:'Due',
+        shipping:'Uncompleted'
+    },
+        
+    {
+        productName:'Admin Dashboard',
+        productNumber:'65940',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+        
+    {
+        productName:'Portfolio',
+        productNumber:'61023',
+        paymentStatus:'Due',
+        shipping:'Uncompleted'
+    },                        
+]
